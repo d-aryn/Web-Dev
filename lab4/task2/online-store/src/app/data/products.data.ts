a66ec780810e73cb5a3dadb196d78f3623b47821
d-aryn <darynnursadyk0@gmail.com>
2026-02-25 10:48:41 +0500

    lab4

---
+++ lab4/task2/online-store/src/app/data/products.data.ts
@@ -0,0 +1,134 @@
+import { Product } from '../models/product.model';
+
+export const PRODUCTS: Product[] = [
+  {
+    id: 1,
+    name: 'Боксерка',
+    description: 'Боксерка Venum',
+    price: 15000,
+    rating: 4.8,
+    image: 'https://resources.cdn-kaspi.kz/img/m/p/pfa/p9e/16331594.jpg?format=preview-large',
+    images: [
+      'https://resources.cdn-kaspi.kz/img/m/p/pfa/p9e/16331594.jpg?format=preview-large',
+      'https://resources.cdn-kaspi.kz/img/m/p/hac/h84/86201657229342.jpg?format=gallery-medium',
+      'https://resources.cdn-kaspi.kz/img/m/p/h03/he8/86201657360414.jpg?format=gallery-medium',
+    ],
+    link: 'https://kaspi.kz/shop/p/bokserki-chernyi-119853159/?c=750000000&utm_source=google&utm_medium=cpc&utm_campaign=shop_google_search_car_automnotive_equipments_v2&gbraid=0AAAAAC7-v7jiYy1KeeXws5EX0YrQymcsk&gclid=CjwKCAiAwNDMBhBfEiwAd7ti1JroFR-Myi3tuNfPKt6pP7hsFF0b_Hoq4y6AGaFcqz5WoPYvMxGgehoCPGcQAvD_BwE',
+  },
+  {
+    id: 2,
+    name: 'Перчатка боксерская',
+    description: 'Бренд IBA',
+    price: 17764,
+    rating: 4.8,
+    image: 'https://resources.cdn-kaspi.kz/img/m/p/p97/p99/89582316.jpeg?format=preview-large',
+    images: [
+      'https://resources.cdn-kaspi.kz/img/m/p/p97/p99/89582316.jpeg?format=preview-large',
+      'https://resources.cdn-kaspi.kz/img/m/p/p23/p68/15813989.jpeg?format=preview-large',
+      'https://resources.cdn-kaspi.kz/img/m/p/p1e/pad/63245738.png?format=preview-large',
+    ],
+    link: 'https://kaspi.kz/shop/p/bokserskie-perchatki-iba-10-oz-belyi-153067196/?c=750000000&utm_source=google&utm_medium=cpc&utm_campaign=shop_google_search_car_automnotive_equipments_v2&gbraid=0AAAAAC7-v7jiYy1KeeXws5EX0YrQymcsk&gclid=CjwKCAiAwNDMBhBfEiwAd7ti1JroFR-Myi3tuNfPKt6pP7hsFF0b_Hoq4y6AGaFcqz5WoPYvMxGgehoCPGcQAvD_BwE'
+  },
+  {
+    id: 3,
+    name: 'Шлем',
+    description: 'Wesing шлем',
+    price: 14500,
+    rating: 5.0,
+    image: 'https://resources.cdn-kaspi.kz/img/m/p/h6d/h56/85615018672158.jpg?format=preview-large',
+    images: [
+      'https://resources.cdn-kaspi.kz/img/m/p/h9b/he4/85631757582366.png?format=preview-large',
+      'https://resources.cdn-kaspi.kz/img/m/p/h6d/h56/85615018672158.jpg?format=preview-large',
+      'https://resources.cdn-kaspi.kz/img/m/p/h6d/h56/85615018672158.jpg?format=preview-large',
+    ],
+    link: 'https://resources.cdn-kaspi.kz/img/m/p/h9b/he4/85631757582366.png?format=preview-large'
+  },
+   {
+    id: 4,
+    name: 'Футбольная форма',
+    description: 'Футболка манчестер сити',
+    price: 14500,
+    rating: 5.0,
+    image: 'https://resources.cdn-kaspi.kz/img/m/p/h6d/h56/85615018672158.jpg?format=preview-large',
+    images: [
+     'https://resources.cdn-kaspi.kz/img/m/p/p55/p97/95681819.png?format=gallery-medium',
+      'https://resources.cdn-kaspi.kz/img/m/p/pc1/p99/95681820.png?format=gallery-medium',
+      'https://resources.cdn-kaspi.kz/img/m/p/pf0/pa2/95684282.png?format=preview-large'
+    ],
+    link: 'https://kaspi.kz/shop/p/futbolka-fk-manchester-siti-domashnjaja-forma-goluboi-46-154673350/?c=750000000&utm_source=google&utm_medium=cpc&utm_campaign=shop_google_search_car_automnotive_equipments_v2&gbraid=0AAAAAC7-v7jiYy1KeeXws5EX0YrQymcsk&gclid=CjwKCAiAwNDMBhBfEiwAd7ti1JroFR-Myi3tuNfPKt6pP7hsFF0b_Hoq4y6AGaFcqz5WoPYvMxGgehoCPGcQAvD_BwE'
+  },
+    {
+    id: 5,
+    name: 'Перчатка для единоборств',
+    description: 'Черепашка ММА',
+    price: 5999,
+    rating: 5.0,
+    image: 'https://resources.cdn-kaspi.kz/img/m/p/p93/pf9/51716963.jpeg?format=gallery-medium',
+    images: [
+     'https://resources.cdn-kaspi.kz/img/m/p/p93/pf9/51716963.jpeg?format=gallery-medium',
+      'https://resources.cdn-kaspi.kz/img/m/p/p29/pf6/51716964.jpeg?format=gallery-medium'
+    ],
+    link: 'https://kaspi.kz/shop/p/rdx-cherepashki-dlja-mma-krasnyi-xl-142138822/?c=750000000&utm_source=google&utm_medium=cpc&utm_campaign=shop_google_search_car_automnotive_equipments_v2&gbraid=0AAAAAC7-v7jiYy1KeeXws5EX0YrQymcsk&gclid=CjwKCAiAwNDMBhBfEiwAd7ti1JroFR-Myi3tuNfPKt6pP7hsFF0b_Hoq4y6AGaFcqz5WoPYvMxGgehoCPGcQAvD_BwE'
+  },
+   {
+    id: 6,
+    name: 'Ражгард',
+    description: 'Ражгард ММА',
+    price: 18000,
+    rating: 2.0,
+    image: 'https://resources.cdn-kaspi.kz/img/m/p/pb1/p73/97213963.jpeg?format=gallery-medium',
+    images: [
+     'https://resources.cdn-kaspi.kz/img/m/p/pb1/p73/97213963.jpeg?format=gallery-medium',
+    ],
+    link: 'https://kaspi.kz/shop/p/rashgard-green-krasnyi-155036774/?c=750000000&utm_source=google&utm_medium=cpc&utm_campaign=shop_google_search_car_automnotive_equipments_v2&gbraid=0AAAAAC7-v7jiYy1KeeXws5EX0YrQymcsk&gclid=CjwKCAiAwNDMBhBfEiwAd7ti1JroFR-Myi3tuNfPKt6pP7hsFF0b_Hoq4y6AGaFcqz5WoPYvMxGgehoCPGcQAvD_BwE'
+  },
+    {
+    id: 7,
+    name: 'Шитки',
+    description: 'Щитки Venkaraxl черный XL',
+    price: 1894,
+    rating: 5.0,
+    image: 'https://resources.cdn-kaspi.kz/img/m/p/h54/h9f/70254687256606.jpg?format=gallery-medium',
+    images: [
+     'https://resources.cdn-kaspi.kz/img/m/p/h54/h9f/70254687256606.jpg?format=gallery-medium',
+    ],
+    link: 'https://kaspi.kz/shop/p/venkaraxl-chernyi-xl-109435704/?c=750000000&utm_source=google&utm_medium=cpc&utm_campaign=shop_google_search_car_automnotive_equipments_v2&gbraid=0AAAAAC7-v7jiYy1KeeXws5EX0YrQymcsk&gclid=CjwKCAiAwNDMBhBfEiwAd7ti1JroFR-Myi3tuNfPKt6pP7hsFF0b_Hoq4y6AGaFcqz5WoPYvMxGgehoCPGcQAvD_BwE'
+  },
+  {
+    id: 8,
+    name: 'Кимано',
+    description: 'Кимоно для карате белый 150',
+    price: 4993,
+    rating: 4.5,
+    image: 'https://resources.cdn-kaspi.kz/img/m/p/p00/pda/63394180.png?format=gallery-medium',
+    images: [
+     'https://resources.cdn-kaspi.kz/img/m/p/p00/pda/63394180.png?format=gallery-medium',
+    ],
+    link: 'https://kaspi.kz/shop/p/kimono-dlja-karate-468365587-belyi-150-145369916/?c=750000000&utm_source=google&utm_medium=cpc&utm_campaign=shop_google_search_car_automnotive_equipments_v2&gbraid=0AAAAAC7-v7jiYy1KeeXws5EX0YrQymcsk&gclid=CjwKCAiAwNDMBhBfEiwAd7ti1JroFR-Myi3tuNfPKt6pP7hsFF0b_Hoq4y6AGaFcqz5WoPYvMxGgehoCPGcQAvD_BwE'},
+    {
+    id: 9,
+    name: 'Шитки',
+    description: 'Щитки Venkaraxl черный XL',
+    price: 1894,
+    rating: 5.0,
+    image: 'https://resources.cdn-kaspi.kz/img/m/p/pee/p1c/5247417.jpg?format=gallery-medium',
+    images: [
+     'https://resources.cdn-kaspi.kz/img/m/p/pee/p1c/5247417.jpg?format=gallery-medium',
+     'https://resources.cdn-kaspi.kz/img/m/p/p0a/p1d/5247418.jpg?format=gallery-medium'
+    ],
+    link: 'https://kaspi.kz/shop/p/bortsovki-chernyi-129084369/?c=750000000&utm_source=google&utm_medium=cpc&utm_campaign=shop_google_search_car_automnotive_equipments_v2&gbraid=0AAAAAC7-v7jiYy1KeeXws5EX0YrQymcsk&gclid=CjwKCAiAwNDMBhBfEiwAd7ti1JroFR-Myi3tuNfPKt6pP7hsFF0b_Hoq4y6AGaFcqz5WoPYvMxGgehoCPGcQAvD_BwE'
+  },
+   {
+    id: 10,
+    name: 'Борцовка',
+    description: 'Борцовка черный',
+    price: 22994,
+    rating: 5.0,
+    image: '',
+    images: [
+     'https://resources.cdn-kaspi.kz/img/m/p/pef/p74/3017105.jpeg?format=gallery-medium',
+     'https://resources.cdn-kaspi.kz/img/m/p/p85/p71/3017104.jpeg?format=gallery-medium'
+    ],
+    link: 'https://kaspi.kz/shop/p/bortsovki-chernyi-128487546/?c=750000000&utm_source=google&utm_medium=cpc&utm_campaign=shop_google_search_car_automnotive_equipments_v2&gbraid=0AAAAAC7-v7jiYy1KeeXws5EX0YrQymcsk&gclid=CjwKCAiAwNDMBhBfEiwAd7ti1JroFR-Myi3tuNfPKt6pP7hsFF0b_Hoq4y6AGaFcqz5WoPYvMxGgehoCPGcQAvD_BwE'
+  },
+];
